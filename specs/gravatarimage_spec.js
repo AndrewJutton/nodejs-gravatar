9da8c9f130fdcbbc363492734b7ccafee2cf66bd
AndrewJutton <ajutton@gmail.com>
2013-08-25 13:30:54 +0100

    fixing module.exports

--- specs/gravatarimage_spec.js
+++ specs/gravatarimage_spec.js
@@ -1,13 +1,13 @@
 should = require('should');
-gravatarImage = require('../lib/gravatarimage');
+gravatar = require('../lib/gravatarImage');
 
 describe("gravatar image tests", function() {
 
     it("should return baseurl with encoded email address appended", function() {
-        gravatarImage.imageUrl('test@gmail.com').should.be.equal('http://www.gravatar.com/avatar/1aedb8d9dc4751e229a335e371db8058');
+        gravatar.imageUrl('test@gmail.com').should.be.equal('http://www.gravatar.com/avatar/1aedb8d9dc4751e229a335e371db8058');
     });
 
     it("should return baseurl with encoded email address appended and size querystring", function() {
-        gravatarImage.imageUrl('test@gmail.com', { "size": 200 }).should.be.equal('http://www.gravatar.com/avatar/1aedb8d9dc4751e229a335e371db8058?size=200');
+        gravatar.imageUrl('test@gmail.com', { "size": 200 }).should.be.equal('http://www.gravatar.com/avatar/1aedb8d9dc4751e229a335e371db8058?size=200');
     });
 });
